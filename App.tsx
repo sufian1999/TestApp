8f4f7da1a57b366dc54c46d60001d910325b4c59
Sufian Ahmad <105508290+sufian1999@users.noreply.github.com>
2025-08-10 00:13:43 +0500

    Changes!
    1. NewArch: false
    2. Add patch lib.
    3. Apply patch in react-native-keyboard-controller to fix issue bottom space.
    4. Add Modal code.
    5. After applying keyboard controller, it starts showing top and bottom spaces.

--- App.tsx
+++ App.tsx
@@ -6,16 +6,51 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { Button, StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import ConfirmModal from './src/components/ConfirmModal';
+import { useState } from 'react';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
 
 function App() {
+
   const isDarkMode = useColorScheme() === 'dark';
 
-  return (
-    <View style={styles.container}>
+  const [showSignOutAlert, setShowSignOutAlert] = useState<boolean>(false);
+
+  const alertData = [
+    {
+      title: 'Confirmation',
+      body: `Are you sure? You want to Sign Out`,
+      btn1: 'Sign Out',
+      btn2: 'Cancel',
+    }
+  ]
+
+  const NewAppScreenComp = () => {
+    return (
+      <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Button
+        onPress={() => setShowSignOutAlert(true)}
+        title="Sign Out"
+      />
       <NewAppScreen templateFileName="App.tsx" />
+
+      <ConfirmModal
+        visible={showSignOutAlert}
+        data={alertData[0]}
+        unVisibal={() => setShowSignOutAlert(false)}
+        btn1={() => setShowSignOutAlert(false)}
+        btn2={() => setShowSignOutAlert(false)}
+      />
     </View>
+    )
+  }
+
+  return (
+    <KeyboardProvider>
+      <NewAppScreenComp />
+    </KeyboardProvider>
   );
 }
 
---
+++ src/components/ConfirmModal.tsx
@@ -0,0 +1,115 @@
+import { StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View, Modal } from 'react-native'
+import React, { } from 'react'
+
+export default function ConfirmModal(props: any) {
+
+
+    let option = props.data
+
+    return (
+        <View>
+
+            <Modal
+                supportedOrientations={['portrait', 'landscape']}
+                transparent={true}
+                visible={props.visible}
+                style={{ flex: 1, width: '100%', height: '100%', }}
+            >
+
+                <TouchableWithoutFeedback onPress={() => props?.unVisibal()}>
+
+                    <View style={styles.mainView}>
+
+                        <TouchableOpacity style={[styles.insideView, { position: 'relative', }]} activeOpacity={1}>
+
+                            <TouchableOpacity onPress={() => props.unVisibal()} style={styles.crossIcon}>
+
+                                <Text>X</Text>
+
+                            </TouchableOpacity>
+
+                            <View>
+                                <Text style={[styles.modalTitleText, { fontSize: 12 }]}>{option?.title} <Text style={[styles.modalBodyText, { fontWeight: 'normal', fontSize: 12, color: '#000' }]}>{option?.body1}</Text></Text>
+
+                                <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                                    <Text style={[styles.modalTitleText, { fontSize: 11, color: 'gray', fontWeight: 'normal', left: 5 }]}>{option?.body}</Text>
+                                </View>
+
+                            </View>
+
+                            <TouchableOpacity
+                                style={[styles.btn, { backgroundColor: props.btn1Color ? props.btn1Color : 'red' }]}
+                                onPress={() => props.btn1()}
+                            >
+                                <Text style={styles.btnText}>{option?.btn1}</Text>
+
+                            </TouchableOpacity>
+
+                            {option?.btn2 &&
+
+                                <TouchableOpacity
+                                    style={[styles.btn, { backgroundColor: props.btn2Color ? props.btn2Color : '#E7E7E9' }]}
+                                    onPress={() => props.btn2()}
+                                >
+                                    <Text style={[styles.btnText, { color: props.ButtonTextColor ? props.ButtonTextColor : '#8A8A8A' }]}>{option?.btn2}</Text>
+
+                                </TouchableOpacity>
+
+                            }
+
+                        </TouchableOpacity>
+
+                    </View>
+
+                </TouchableWithoutFeedback>
+
+            </Modal>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    mainView: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    },
+    insideView: {
+        width: '100%',
+        padding: 30,
+        backgroundColor: '#F2F2F8',
+        elevation: 6,
+    },
+    modalTitleText: {
+        textAlign: 'left',
+        fontWeight: 'bold',
+        fontSize: 16,
+        color: 'black',
+    },
+    modalBodyText: {
+        fontSize: 14,
+        color: '#8A8A8A',
+        marginVertical: 10
+    },
+    btn: {
+        width: '100%',
+        padding: 15,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
+        marginTop: 10
+    },
+    btnText: {
+        color: '#fff',
+        fontSize: 14,
+        fontWeight: 'bold',
+        textTransform: 'uppercase'
+    },
+    crossIcon: {
+        position: 'absolute',
+        top: -30,
+        left: 8
+    }
+})
